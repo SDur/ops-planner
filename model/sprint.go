3ca6c9e2586fbcce6f138cf384aebed1eaffe553
during <sjoerd.during@jcore.com>
2019-03-26 17:07:43 +0100

    Changed number into nr for mapping

--- model/sprint.go
+++ model/sprint.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Sprint struct {
-	Id     int64
-	Number int64
-	Start  time.Time
-	Days   [10]int
+	Id    int64
+	Nr    int64
+	Start time.Time
+	Days  [10]int
 }
